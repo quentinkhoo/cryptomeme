7208d8ba7c2b4d320b426653d023e36fef1ac740
TzeMeng <TzeMeng@users.noreply.github.com>
2020-04-02 18:01:31 +0800

    Contract deployed

--- truffle-config.js
+++ truffle-config.js
@@ -15,17 +15,21 @@
 // };
 
 let secrets = require('./secrets');
-const WalletProvider = require("truffle-wallet-provider");
-const Wallet = require('ethereumjs-wallet');
+var mem = secrets.meta;
+var HDWalletProvider = require("truffle-hdwallet-provider")
+// const WalletProvider = require("truffle-wallet-provider");
+// const Wallet = require('ethereumjs-wallet');
 
-let ropstenPrivateKey = new Buffer(secrets.ropstenPK, "hex");
-let ropstenWallet = Wallet.fromPrivateKey(ropstenPrivateKey);
-let ropstenProvider = new WalletProvider(ropstenWallet,  "https://ropsten.infura.io/v3/b83df1f175b14580b49eba109101107d");
+// let ropstenPrivateKey = new Buffer(secrets.ropstenPK, "hex");
+// let ropstenWallet = Wallet.fromPrivateKey(ropstenPrivateKey);
+// let ropstenProvider = new WalletProvider(mem,  "https://ropsten.infura.io/v3/b83df1f175b14580b49eba109101107d");
 module.exports = {
   networks: {
     development: { host: "localhost", port: 8545, 
                    network_id: "*", gas: 4465030 },    
-    ropsten: { provider: ropstenProvider, 
+    ropsten: { provider: function() {
+      return new HDWalletProvider(mem,"https://ropsten.infura.io/v3/b83df1f175b14580b49eba109101107d")
+    }, 
                    network_id: "3", gas: 4465030 }
   }
 };
